8bc821000f302a2c8ca508eab7750acb96dcbe96
Evangilo Morais <evangilo@evolux.net.br>
2019-08-07 13:11:49 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from setuptools import find_packages, setup
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(name='sqlalchemyp',
       version='0.0.1',
       description='SQLAlchemy Print Query',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       author='Evangilo',
       author_email='evangilo@gmail.com',
       packages=find_packages(exclude=('tests', 'docs', 'build')),
-      install_requires=['sqlalchemy', 'sqlparse']
-      )
+      install_requires=['sqlalchemy', 'sqlparse'],
+      license=u'MIT',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ])
